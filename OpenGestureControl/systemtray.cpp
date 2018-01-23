f0f69571c9444dcff92d21d7e2186ffb580a314d
Sylvia van Os <sylvia.van.os@ict.nl>
2018-01-23 09:50:33 +0100

    Remove broken testing functionality

--- OpenGestureControl/systemtray.cpp
+++ OpenGestureControl/systemtray.cpp
@@ -27,10 +27,7 @@ SystemTray::SystemTray(QObject *parent) : QObject(parent)
     this->tray = new QSystemTrayIcon(QIcon(":/icons/app.png"), qApp);
 
     this->trayMenu = new QMenu();
-    trayMenu->addAction(tr("Open menu"));
     trayMenu->addAction(tr("&Quit"), qApp, &QApplication::quit);
-
-    connect(this->trayMenu, SIGNAL(triggered(QAction*)), this, SLOT(menuActionClicked(QAction*)));
 
     this->tray->setContextMenu(this->trayMenu);
 }
@@ -43,11 +40,4 @@ void SystemTray::show() const
 void SystemTray::showMessage(const QString message) const
 {
     this->tray->showMessage(tr("OpenGestureControl"), message);
-}
-
-void SystemTray::menuActionClicked(const QAction *action) const
-{
-    if (action->text() == tr("Open menu")) {
-        emit menuOpenClicked();
-    }
 }
